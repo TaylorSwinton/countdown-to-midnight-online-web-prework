b7a9916a2faaeddd49eef81d5df1aa7875afdaf5
Taylor Swinton <taylorswinton1@gmail.com>
2019-01-07 22:24:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,14 @@
 #write your code here
 
 def countdown(x)
+  number = x
+    while condition > 0 
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
 
+def countdown_with_sleep(x)
+  
 end
